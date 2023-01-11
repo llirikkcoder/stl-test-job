c4445e663a08d05bd7a1a909c51056f6125c98ec
Kipol <llirikk@gmail.com>
2023-01-11 15:08:59 +0400

    in progress

--- src/components/UsersTable.jsx
+++ src/components/UsersTable.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
+
 const Table = styled.table`
   width: 100%;
   border-collapse: collapse;
@@ -13,10 +14,17 @@ const Table = styled.table`
   }
 `;
 
-
 function UsersTable() {
   const [users, setUsers] = useState([]);
   const [sortField, setSortField] = useState(null);
+  const [editingUser, setEditingUser] = useState(null);
+
+  const [newUser, setNewUser] = useState({
+    username: "",
+    email: "",
+    age: "",
+    country: ""
+  });
 
   useEffect(() => {
     async function fetchUsers() {
@@ -25,7 +33,7 @@ function UsersTable() {
       setUsers(users);
     }
     fetchUsers();
-  }, []);
+  }, [editingUser]);
 
   const handleSort = (field) => {
     if (sortField === field) {
@@ -45,36 +53,196 @@ function UsersTable() {
     setUsers(sortedUsers);
   };
 
+  const handleNewUserChange = (e) => {
+    setNewUser({
+      ...newUser,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleEditUserChange = (e) => {
+    setEditingUser({
+      ...editingUser,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleEditUser = (user) => {
+    setEditingUser({ ...user });
+  };
+
+  const handleDeleteUser = (user) => {
+    async function deleteUser(user) {
+      await fetch(`http://localhost:3001/users/${user.id}`, {
+        method: "DELETE"
+      });
+      const updatedUsers = users.filter((u) => u.id !== user.id);
+      setUsers(updatedUsers);
+    }
+    deleteUser(user);
+  };
+
+  const handleSaveUser = (user) => {
+    async function saveUser(user) {
+      let response;
+      if (user.id) {
+        // update existing user
+        response = await fetch(`http://localhost:3001/users/${user.id}`, {
+          method: "PUT",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(user)
+        });
+      } else {
+        // create new user
+        response = await fetch("http://localhost:3001/users", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(user)
+        });
+      }
+      const savedUser = await response.json();
+      const updatedUsers = user.id
+        ? users.map((u) => (u.id === savedUser.id ? savedUser : u))
+        : [...users, savedUser];
+      setUsers(updatedUsers);
+    }
+    saveUser(user);
+  };
+
+  const handleCancelEdit = () => {
+    setEditingUser(null);
+  };
+
+  const handleCreateUser = () => {
+    handleSaveUser(newUser);
+    setNewUser({
+      username: "",
+      email: "",
+      age: "",
+      country: ""
+    });
+  };
+
   return (
     <Table>
-      <table>
-        <thead>
-          <tr>
-            <th>
-              <button onClick={() => handleSort('username')}>Username</button>
-            </th>
-            <th>
-              <button onClick={() => handleSort('email')}>Email</button>
-            </th>
-            <th>
-              <button onClick={() => handleSort('age')}>Age</button>
-            </th>
-            <th>
-              <button onClick={() => handleSort('country')}>Country</button>
-            </th>
+      {/* <table> */}
+      <thead>
+        <tr>
+          <th>
+            <button onClick={() => handleSort('username')}>Username</button>
+          </th>
+          <th>
+            <button onClick={() => handleSort('email')}>Email</button>
+          </th>
+          <th>
+            <button onClick={() => handleSort('age')}>Age</button>
+          </th>
+          <th>
+            <button onClick={() => handleSort('country')}>Country</button>
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        {users.map((user) => (
+          <tr key={user.id}>
+            <td>
+              {editingUser?.id === user.id ? (
+                <input
+                  name="username"
+                  value={editingUser?.username || ""}
+                  onChange={handleEditUserChange}
+                />
+              ) : (
+                user.username || ""
+              )}
+            </td>
+            <td>
+              {editingUser?.id === user.id ? (
+                <input
+                  name="email"
+                  value={editingUser?.email}
+                  onChange={handleEditUserChange}
+                />
+              ) : (
+                user.email
+              )}
+            </td>
+            <td>
+              {editingUser?.id === user.id ? (
+                <input
+                  name="age"
+                  value={editingUser?.age}
+                  onChange={handleEditUserChange}
+                />
+              ) : (
+                user.age
+              )}
+            </td>
+            <td>
+              {editingUser?.id === user.id ? (
+                <input
+                  name="country"
+                  value={editingUser?.country}
+                  onChange={handleEditUserChange}
+                />
+              ) : (
+                user.country
+              )}
+            </td>
+            <td>
+              {editingUser?.id === user.id ? (
+                <>
+                  <button onClick={() => handleSaveUser(editingUser)}>
+                    Save
+                  </button>
+                  <button onClick={handleCancelEdit}>Cancel</button>
+                </>
+              ) : (
+                <>
+                  <button onClick={() => handleEditUser(user)}>Edit</button>
+                  <button onClick={() => handleDeleteUser(user)}>Delete</button>
+                </>
+              )}
+            </td>
           </tr>
-        </thead>
-        <tbody>
-          {users.map((user) => (
-            <tr key={user.id}>
-              <td>{user.username}</td>
-              <td>{user.email}</td>
-              <td>{user.age}</td>
-              <td>{user.country}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+        ))}
+      </tbody>
+      <tfoot>
+        <tr>
+          <td>
+            <input
+              name="username"
+              value={newUser?.username}
+              onChange={handleNewUserChange}
+            />
+          </td>
+          <td>
+            <input
+              name="email"
+              value={newUser?.email}
+              onChange={handleNewUserChange}
+            />
+          </td>
+          <td>
+            <input
+              name="age"
+              value={newUser?.age}
+              onChange={handleNewUserChange}
+            />
+          </td>
+          <td>
+            <input
+              name="country"
+              value={newUser?.country}
+              onChange={handleNewUserChange}
+            />
+          </td>
+          <td>
+            <button onClick={handleCreateUser}>Create</button>
+          </td>
+        </tr>
+      </tfoot>
+      {/* </table> */}
     </Table>
   );
 }
